bb76750116b7a453ecf128eca07a0978a9d10ea6
Inferno <nishantkatekar58@gmail.com>
2023-09-12 19:16:34 +0530

    user crud

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const app=express();
 const dotenv=require("dotenv").config()
 const mongoose=require("mongoose")
 const authRoute=require('./routes/auth');
+const userRoute=require('./routes/users');
 
 app.use(express.json());
 
-mongoose
+mongoose 
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -14,7 +15,8 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
 
-    app.use("/api/auth",authRoute)
+    app.use("/api/auth",authRoute) 
+    app.use("/api/users",userRoute) 
 
 app.listen("5000",()=>{
     console.log(`backend is running on PORT 5000`);
